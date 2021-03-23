f9c342e0aca052a98313fab0d8aac1c8cb91d996
Zachary Jayes <zacjayes@gmail.com>
2021-03-22 21:16:12 -0500

    finished webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,30 @@
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 
+const config = {
+    entry: "./public/index.js",
+    output: {
+        path: __dirname + "/dist",
+        filename: "bundle.js"
+    },
+    mode: "",
+    pluggins: [
+        new WebpackPwaManifest({
+           fingerprints: false,
+           name: "Budget Tracker",
+           short_name: "Budget App",
+           description:"An application that allows you to create and track a budget.",
+           theme_color: '#ffffff',
+           'theme-color': '#ffffff',
+           start_url: '/', 
+           icons: [
+               {
+                   src: path.resolve("public/icons/icon-192x192.png"),
+                   size: [192, 512]
+               }
+           ]
+        })
+    ]
+}
+
+module.exports = config;
